22233600b88877d065836a59bb2a8308c00c4e86
Ashley Muller <serenehopes@gmail.com>
2018-05-24 01:59:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  
+  yearsExperienceFromBeginningOf(year){
+    let endDate = new Date(year, 1, 1)
+    let total = (endDate - this.startDate)/(365*24*60*60*1000) + 1
+    return parseInt(total)  
+  }
+}
 
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+}
